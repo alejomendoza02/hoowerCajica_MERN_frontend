e9dce3c22f149b8beecccf41b460c95c89500f30
Alejandro <alejomendoza0202@gmail.com>
2022-04-28 11:59:33 -0500

    Rename the images

--- src/pages/Renacer.jsx
+++ src/pages/Renacer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import ImageHeader from "../img/biografia/renacer.png";
 import Img1 from "../img/renacer/img1.jpg";
-import Img2 from "../img/renacer/img2.jpg";
+import Img2 from "../img/renacer/img2.JPG";
 import Img3 from "../img/renacer/img3.jpg";
 
 const Renacer = () => {
