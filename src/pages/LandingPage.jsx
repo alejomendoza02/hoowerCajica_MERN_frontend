0a91ff211b092d25fbbe6afaaebbf695dbf93e01
Alejandro <alejomendoza0202@gmail.com>
2022-04-27 21:50:59 -0500

    Fixing a bug

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,6 +10,17 @@ const LandingPage = () => {
     <>
       <Helmet>
         <title>{`Inicio | Hoower Cajicá`}</title>
+        <meta property="og:title" content="Hoower Cajicá" />
+        <meta
+          property="og:image"
+          content="https://yt3.ggpht.com/ytc/AKedOLQhleKl-s_qkN9MUER2fgwCjhG07jwFDEtLdm4R=s900-c-k-c0x00ffffff-no-rj"
+        ></meta>
+        <meta
+          property="og:description"
+          content="El siitio web oficial de Hoower Cajicá."
+        ></meta>
+        <meta property="og:url" content="https://www.hoowercajica.com"></meta>
+        <meta property="og:type" content="website"></meta>
       </Helmet>
       <div className="h-full w-full">
         <Slideshow />
