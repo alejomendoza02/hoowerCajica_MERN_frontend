cb9b9848f8543f8d52e6ff2dd49bc2440a36358b
Alejandro <alejomendoza0202@gmail.com>
2022-05-08 17:53:27 -0500

    fixing buttons clickarea

--- src/config/clientAxios.jsx
+++ src/config/clientAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
+    baseURL: import.meta.env.MODE === 'development' ? `${import.meta.env.VITE_BACKEND_URL}/api` : `${import.meta.env.VITE_BACKEND_URL_PRODUCTION}/api`
 }) 
 
 export default clienteAxios;
